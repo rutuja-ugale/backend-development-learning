b33e9db8ec7aa5e2fbacd44c7e5826aa19374a9d
rutujaa121 <rutujaaugale@gmail.com>
2026-05-29 11:21:59 +0530

    create simple react app

---
+++ 04-React_Js/day-01/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React JS</h1>
+      <p>This is a simple React application.</p>
+      <p>React allows you to build reusable UI components and manage the state of your application efficiently.</p>
+    </div>
+  );
+}
+
+export default App;

f4e0d36940e6315509ed1a47fa7757c0ffea64d2
rutujaa121 <rutujaaugale@gmail.com>
2026-05-29 11:28:30 +0530

    update folder

---
+++ 04-React_Js/day-02/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React JS</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
